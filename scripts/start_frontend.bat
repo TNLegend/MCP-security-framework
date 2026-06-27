5803043f57c230cbdcf77aefd5231cd8882109c3
R I B <mohamed-yassine.abich@etu.univ-amu.fr>
2026-06-27 11:05:05 +0200

    Fix frontend startup script on Windows

--- scripts/start_frontend.bat
+++ scripts/start_frontend.bat
@@ -1,7 +1,12 @@
 @echo off
-cd /d "%~dp0..\frontend"
+setlocal
 
-npm install
-npm run dev
+cd /d "%~dp0..\frontend" || exit /b 1
+
+echo Installing frontend dependencies...
+call npm install || exit /b 1
+
+echo Starting frontend development server...
+call npm run dev
 
 pause
